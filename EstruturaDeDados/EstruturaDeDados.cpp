d9088e0aa6c023c76990e8f69dfacd05e29a1e4c
icidade <icidade@gmail.com>
2015-09-23 22:42:49 -0300

    post do forum B

--- EstruturaDeDados/EstruturaDeDados.cpp
+++ EstruturaDeDados/EstruturaDeDados.cpp
@@ -12,7 +12,36 @@ int funcao(float v[], float vl, int n)
 	return -1;
 }
 
+struct cadastro_endereco {
+	char nome[20]; //guarda o nome da filial
+	float latitude; // guarda a latitude da loja
+	float longitude; // guarda a longitude da loja
+} favorito[5];
+
+void preenche_lista(cadastro_endereco favs[], int n) {
+	for (int i = 0; i < n; i++) {
+		cout << "\nFavorito " << i + 1;
+		cout << "\n\tNome: ";cin>> favs[i].nome;
+		cout << "\n\tLatitude: ";cin>> favs[i].latitude;
+		cout << "\n\tLongitude: ";cin>> favs[i].longitude;
+	}
+}
+
+void exibe_lista(cadastro_endereco favs[], int n) {
+	cout << "\nExibindo itens da lista de favoritos" << endl;
+	for (int i = 0; i < n; i++) {
+		cout << "\nFavorito " << i + 1;
+		cout << "\n\tNome: " << favs[i].nome;
+		cout << "\n\tLatitude: " << favs[i].latitude;
+		cout << "\n\tLongitude: " << favs[i].longitude;
+	}
+	cout << "\n\n";
+}
+
 int main() {
+	
+	preenche_lista(favorito, 5);
+	exibe_lista(favorito, 5);
 
 	system("pause");
 }
